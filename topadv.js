0c05bebcb00e011e137f6dcaf22856c1159d9878
Sunil Patro <sunilpatro1985@gmail.com>
2018-01-30 17:52:42 +0530

    Add files via upload

---
+++ topadv.js
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- qavheaderadd2 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-2906531635031835"
+     data-ad-slot="8761565708"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
